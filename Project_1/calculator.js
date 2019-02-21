8ebafc2f09801353707eb937e3ff74b6982c9646
JavierJRRamirez <Javier.Ramirez@colorado.edu>
2019-02-21 00:50:39 -0700

    Edited HTML and Javascript

--- Project_1/calculator.js
+++ Project_1/calculator.js
@@ -2,16 +2,41 @@
          function show(num) 
          { 
              document.getElementById("answer").value+=num
-         } 
-           
+         }
+
          //calculate
          function calculate() 
          { 
              let x = document.getElementById("answer").value 
              let y = document.getElementById("answer").value 
 			 let z = eval(x,y)
-             document.getElementById("answer").value = z 
+             document.getElementById("answer").value = z
          } 
+		 function sin() {
+			let x = document.getElementById("answer").value
+			let z = Math.sin(x)
+             document.getElementById("answer").value = z
+		 }
+		 function cos() {
+			let x = document.getElementById("answer").value
+			let z = Math.cos(x)
+             document.getElementById("answer").value = z
+		 }
+		 function tan() {
+			let x = document.getElementById("answer").value
+			let z = Math.tan(x)
+             document.getElementById("answer").value = z
+		 }
+		 function sqrt() {
+			let x = document.getElementById("answer").value
+			let z = Math.sqrt(x)
+             document.getElementById("answer").value = z
+		 }
+		 function log() {
+			let x = document.getElementById("answer").value
+			let z = Math.log(x)
+             document.getElementById("answer").value = z
+		 }
            
          //clear the display 
          function clr() 
