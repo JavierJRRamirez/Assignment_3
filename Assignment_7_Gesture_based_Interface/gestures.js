624a8b0ad926ec91ab088982ee118b675479462e
JavierJRRamirez <Javier.Ramirez@colorado.edu>
2019-03-18 18:53:29 -0600

    Assignment 7 updated

--- Assignment_7_Gesture_based_Interface/gestures.js
+++ Assignment_7_Gesture_based_Interface/gestures.js
@@ -1,1 +1,26 @@
+downX = 0;
+downY = 0;
+
+$("#gestureArea").mouseup(function() {
+                          $("#gestureResult").text("mouse up");
+                          upX = event.pageX;
+                          upY = event.pageY;
+                          
+                          if (upX < downX) {
+                          $("#gestureResult").text("swipe left");
+                          }
+                          else if (upX > downX){
+                          $("#gestureResult").text("swipe right");
+                          }
+                          else if (upX == downX){
+                          $("#gestureResult").text("mouse up");
+                          }
+
+                          });
+
+$("#gestureArea").mousedown(function() {
+                          $("#gestureResult").text("mouse down");
+                            downX = event.pageX;
+                            downY = event.pageY;
+                          });
 
